5dab0410bbc6a148413a914af7f81b80bf73c89a
Gokuladasan <velugokuladasan@gmail.com>
2022-06-29 16:46:22 +0100

    29062022

--- Velocities.py
+++ Velocities.py
@@ -1,13 +1,28 @@
-def velocities(np):
+import math
 
-    npindex=np+1
+
+def velocities(np, y, z, circulation):
+    pi = math.pi
+    npindex = np+1
 
     u_list = []
-    u_value=0
+    u_value = 0
     u_list.extend([u_value for i in range(1, npindex)])
 
     v_list = []
-    v_value=0
+    v_value = 0
     v_list.extend([v_value for i in range(1, npindex)])
 
-    return v_list, u_list
+    w_list = []
+    w_value = 0
+    w_list.extend([w_value for i in range(1, npindex)])
+
+    for i in range(1, npindex):
+
+        for j in range(1, npindex):
+            r2 = (y(i) - y(j)) ^ 2 + (z(i) - z(j)) ^ 2
+            u_list[j] = u_list[j] + circulation[i] * (z[j] - z[i])/r2/2/pi
+            v_list[j] = v_list[j] - circulation[i] * (z[j] - z[i]) / r2 / 2 / pi
+            w_list = u_list[j] + v_list[j]
+
+    return v_list, u_list, w_list
