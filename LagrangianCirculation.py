fe364b818f8858fc5ae26c1354208ed05cc7dd06
Gokuladasan <velugokuladasan@gmail.com>
2022-06-30 16:37:03 +0100

    30062022

--- LagrangianCirculation.py
+++ LagrangianCirculation.py
@@ -1,6 +1,6 @@
 def lagrangian_circulation(y, b, gammao, np):
     circulation=[]
-
+#changed
     # for i in range(1, (np+1)):
     #     circulation[i] = gammao * y[i] * (1 - ((2 * y[i]) / b) ** 2) ** (-1 / 2)
     #
