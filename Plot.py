0ea5117db70f032c190f1326481b76683de639e8
Gokuladasan <velugokuladasan@gmail.com>
2022-07-06 18:00:58 +0100

    07062022

--- Plot.py
+++ Plot.py
@@ -1,5 +1,29 @@
-def Plot(tmax, dt, np, y, z, circulation):
-    d = 0.1
+from Velocities import velocities
+import matplotlib.pyplot as plt
 
 
-    return
+def plot(d, tmax, dt, np, y, z, circulation):
+    time = (tmax / dt) + 1
+    time = int(time)
+    yy = []
+    zz = []
+    for i in range(1, time):
+        vel = velocities(np, y, z, circulation, d)
+        u = [vel[0]]
+        v = [vel[1]]
+        u = u[(len(u) - 1)]
+        v = v[(len(v) - 1)]
+        for i in range(0, (len(y))):
+            y[i] = y[i] + (u[i] * dt)
+            z[i] = z[i] + (v[i] * dt)
+        yy.append(y)
+        zz.append(z)
+
+    # final plot:
+    plotz = zz[(len(zz) - 1)]
+    ploty = yy[(len(yy) - 1)]
+
+    plt.plot(ploty, plotz)
+    a = plt.show()
+
+    return a
--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import numpy as npy
-
-from LagrangianCirculation import lagrangian_circulation
-from Velocities import velocities
+from Plot import plot
 from VortexCoordinates import vortex_coodinates
-import matplotlib.pyplot as plt
-import numpy as npy
 # %% initial list of points
 # %% number of points
 
@@ -51,29 +46,7 @@ coordinates = vortex_coodinates(np, b, gammao, np)
 y = coordinates[0]
 circulation = coordinates[1]
 z = coordinates[2]
-time = (tmax/dt)+1
-time = int(time)
-yy = []
-zz = []
-for i in range(1, time):
-    vel = velocities(np, y, z, circulation, d)
-    u = [vel[0]]
-    v = [vel[1]]
-    u = u[(len(u)-1)]
-    v = v[(len(v)-1)]
-    for i in range(0, (len(y))):
-        y[i] = y[i] + (u[i]*dt)
-        z[i] = z[i] + (v[i]*dt)
-    yy.append(y)
-    zz.append(z)
-
-# final plot:
-plotz =zz[(len(zz)-1)]
-ploty =yy[(len(yy)-1)]
-
-plt.plot(ploty, plotz)
-plt.show()
-
+plot(d, tmax, dt, np, y, z, circulation)
 
 
 
